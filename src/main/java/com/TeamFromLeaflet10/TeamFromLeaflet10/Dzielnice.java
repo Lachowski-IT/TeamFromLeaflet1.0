95474fa8294ddff26626c62386a8bc7a8def3fb1
Lachowski-IT <lachowski.tomasz@gmail.com>
2020-03-25 11:42:15 +0100

    constructors + getters

--- src/main/java/com/TeamFromLeaflet10/TeamFromLeaflet10/Dzielnice.java
+++ src/main/java/com/TeamFromLeaflet10/TeamFromLeaflet10/Dzielnice.java
@@ -15,4 +15,28 @@ public class Dzielnice {
     private String name;
     private float powierzchnia;
     private int Liczba_Ludnosci;
+
+    protected Dzielnice() {}
+
+    public Dzielnice(String name, float powierzchnia, int liczba_Ludnosci) {
+        this.name = name;
+        this.powierzchnia = powierzchnia;
+        Liczba_Ludnosci = liczba_Ludnosci;
+    }
+
+    public Long getID() {
+        return ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public float getPowierzchnia() {
+        return powierzchnia;
+    }
+
+    public int getLiczba_Ludnosci() {
+        return Liczba_Ludnosci;
+    }
 }
